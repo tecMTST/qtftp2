48457cb48b366dac0667aad32df69c21ff6bb27d
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2025-06-26 21:56:54 -0300

    HUD Nível de bagunça

--- Projeto/UI/Interfaces/hud.gd
+++ Projeto/UI/Interfaces/hud.gd
@@ -1,5 +1,13 @@
 extends Control
-@onready var label: Label = $Tempo/MarginContainer/Label
+@onready var tempo_label: Label = %TempoLabel
+@onready var bagunca_progress_bar: ProgressBar = %BaguncaProgressBar
+
+func _ready() -> void:
+	ControleDeFase.NivelIniciado.connect(_nivel_iniciado)
+
+func _nivel_iniciado(NivelAtual: Nivel, EstadoNivel: EstadoDoNivel) -> void:
+	bagunca_progress_bar.max_value = EstadoNivel.LimiteBagunca
 
 func _process(_delta: float) -> void:
-	label.text = ControleDeFase.EstadoNivel.TempoRestanteFormatado
+	tempo_label.text = ControleDeFase.EstadoNivel.TempoRestanteFormatado
+	bagunca_progress_bar.value = ControleDeFase.EstadoNivel.Bagunca
