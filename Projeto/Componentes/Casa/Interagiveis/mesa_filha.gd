b722a999f92984e4a0d58956a0755eb79953d8de
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2025-07-10 22:32:56 -0300

    Entrega de pratos - Visualizador de numeral de pratos entregues - Realiza a contagem de pratos entregues - Dispara evento de entrega de pratos - Reage ao evento do prato entregue - Ao completar o número de pratos entregues encerra a fase

--- Projeto/Componentes/Casa/Interagiveis/mesa_filha.gd
+++ Projeto/Componentes/Casa/Interagiveis/mesa_filha.gd
@@ -1,11 +1,21 @@
 class_name MesaFilha extends BaseInteragivel
 
+@onready var pivot: Node2D = $Pivot
+
+var objetoAtual : IngredienteBase = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Nome = "Mesa"	
+	Nome = "Mesa"
 
+func _interagir(jogador: Player) -> void:
+	if(objetoAtual == null and jogador.objetoAgarrado is IngredienteBase):
+		if (jogador.objetoAgarrado.pode_entregar()):
+			objetoAtual = jogador.objetoAgarrado
+			jogador.soltar()
+			_posicionarObjeto()
+			objetoAtual.entregar()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _posicionarObjeto() -> void:
+	objetoAtual.reparent(pivot)
+	objetoAtual.global_position = pivot.global_position
