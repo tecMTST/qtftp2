e211f90e5c1e6798ec4d1049fdeac5d353fbd026
Breno G. de Oliveira <garu@cpan.org>
2025-07-23 13:45:12 -0300

    ajustes apos analise estatica

--- Projeto/Componentes/Casa/Interagiveis/berco.gd
+++ Projeto/Componentes/Casa/Interagiveis/berco.gd
@@ -1,25 +1,27 @@
 class_name Berco extends BaseInteragivel
 
+var timer_choro: float = 0
+var iniciado: bool = false
+var choro_ativo: bool = false
+
 @onready var audio_choro: AudioStreamPlayer2D = $AudioChoro
 @onready var bebe: AnimatedSprite2D = $Bebe
 @onready var timer: Timer = $Timer
 @onready var visualizador_temporal: VisualizadorTemporal = $VisualizadorTemporal
 
-var timer_choro : float = 0
-var iniciado : bool = false
-var choro_ativo : bool = false
 
 func _ready() -> void:
-	Nome = "Berço"	
+	Nome = "Berço"
 	if not ControleDeFase.NivelAtual or not ControleDeFase.NivelAtual.Choro:
-		return	
+		return
 	#TODO Configurar Audio do choro
 	resetar_timer()
 	iniciado = true
-	
+
+
 func _process(delta: float) -> void:
 	if not ControleDeFase.NivelAtual or not ControleDeFase.NivelAtual.Choro:
-		return	
+		return
 	if not iniciado:
 		iniciado = true
 		resetar_timer()
@@ -30,12 +32,14 @@ func _process(delta: float) -> void:
 	if timer_choro <= 0:
 		iniciar_choro()
 
-func iniciar_choro():	
+
+func iniciar_choro():
 	#TODO animacao iniciar
 	choro_ativo = true
 	timer.start(ControleDeFase.NivelAtual.TempoLimiteChoro)
-	audio_choro.play()		
-	
+	audio_choro.play()
+
+
 func finalizar_choro():
 	#TODO animacao parar
 	choro_ativo = false
@@ -43,13 +47,20 @@ func finalizar_choro():
 	audio_choro.stop()
 	resetar_timer()
 
+
 func resetar_timer():
 	if ControleDeFase.NivelAtual:
-		timer_choro = ControleDeFase.NivelAtual.IntervaloChoro \
-		+ randf_range(-ControleDeFase.NivelAtual.VariacaoChoro, ControleDeFase.NivelAtual.VariacaoChoro)		
+		timer_choro = (
+			ControleDeFase.NivelAtual.IntervaloChoro
+			+ randf_range(
+				-ControleDeFase.NivelAtual.VariacaoChoro, ControleDeFase.NivelAtual.VariacaoChoro
+			)
+		)
+
 
 func _on_timer_timeout() -> void:
 	ControleDeFase.EstadoNivel.LimiteChoroAtingido = true
 
-func _on_componente_interagivel_interagir(jogador: Player) -> void:
+
+func _on_componente_interagivel_interagir(_jogador: Player) -> void:
 	finalizar_choro()
