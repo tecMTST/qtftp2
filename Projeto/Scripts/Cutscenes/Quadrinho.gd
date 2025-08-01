b23ba206349d500c3ee574c4933a967a0b8ceb09
G-Machado <5076289+G-Machado@users.noreply.github.com>
2025-08-01 00:16:28 -0300

    correções script Quadrinho

--- Projeto/Scripts/Cutscenes/Quadrinho.gd
+++ Projeto/Scripts/Cutscenes/Quadrinho.gd
@@ -1,60 +1,60 @@
 class_name Quadrinho extends Node
 
-@export var posicaoBounceInicial : Node2D
-@export var velocidadeBounce : float = 1
-@export var duracaoFadeAlfa : float = 0
+@export var posicao_bounce_inicial : Node2D
+@export var velocidade_bounce : float = 1
+@export var duracao_fade_alfa : float = 0
 
-var posicaoBounceFinal : Vector2 = Vector2.ZERO
-var quadrinhoSprite : Sprite2D
-var tempoDeFade : float
-var _bounceFinalizado : bool = false
-var _alphaFinalizado : bool = false
+var posicao_bounce_final : Vector2 = Vector2.ZERO
+var quadrinho_sprite : Sprite2D
+var tempo_de_fade : float
+var _bounce_finalizado : bool = false
+var _alpha_finalizado : bool = false
 
 signal OnQuadrinhoFinalizado
 
 func _ready() -> void:
-	quadrinhoSprite = get_node("QuadrinhoSprite")
-	posicaoBounceFinal = quadrinhoSprite.global_position
+	quadrinho_sprite = get_node("QuadrinhoSprite")
+	posicao_bounce_final = quadrinho_sprite.global_position
 
 func AtivarQuadrinho() -> void:
 	self.visible = true
-	if(duracaoFadeAlfa > 0):
-		tempoDeFade = 0
+	if(duracao_fade_alfa > 0):
+		tempo_de_fade = 0
 
 func _process(delta: float) -> void:
 	if(!self.visible): return
-	if(posicaoBounceFinal == Vector2.ZERO): return
+	if(posicao_bounce_final == Vector2.ZERO): return
 	
-	if(posicaoBounceInicial):
+	if(posicao_bounce_inicial):
 		_movimentar_bounce()
 		
-	if(duracaoFadeAlfa > 0):
-		tempoDeFade += delta
+	if(duracao_fade_alfa > 0):
+		tempo_de_fade += delta
 		_atualizar_transparencia()
 
 func Configurar_quadrinho() -> void:
 	self.visible = false
 	
-	if(posicaoBounceInicial):
-		quadrinhoSprite.global_position = posicaoBounceInicial.global_position
+	if(posicao_bounce_inicial):
+		quadrinho_sprite.global_position = posicao_bounce_inicial.global_position
 	
-	_bounceFinalizado = !posicaoBounceInicial
-	_alphaFinalizado = duracaoFadeAlfa <= 0
+	_bounce_finalizado = !posicao_bounce_inicial
+	_alpha_finalizado = duracao_fade_alfa <= 0
 
 func _movimentar_bounce() -> void:
-	var vetorDelta = posicaoBounceFinal - quadrinhoSprite.global_position
-	var vetorFinal = vetorDelta * (velocidadeBounce/10)
-	quadrinhoSprite.global_position += vetorFinal
-	if(vetorDelta.length() < .15 and !_bounceFinalizado):
-		_bounceFinalizado = true
+	var vetorDelta = posicao_bounce_final - quadrinho_sprite.global_position
+	var vetorFinal = vetorDelta * (velocidade_bounce/10)
+	quadrinho_sprite.global_position += vetorFinal
+	if(vetorDelta.length() < .15 and !_bounce_finalizado):
+		_bounce_finalizado = true
 		_checkSeFinalizado()
 
 func _atualizar_transparencia() -> void:
-	quadrinhoSprite.modulate.a = tempoDeFade / duracaoFadeAlfa
-	if(tempoDeFade / duracaoFadeAlfa >= .9 and !_alphaFinalizado):
-		_alphaFinalizado = true
+	quadrinho_sprite.modulate.a = tempo_de_fade / duracao_fade_alfa
+	if(tempo_de_fade / duracao_fade_alfa >= .9 and !_alpha_finalizado):
+		_alpha_finalizado = true
 		_checkSeFinalizado()
 
 func _checkSeFinalizado() -> void:
-	if(_alphaFinalizado && _bounceFinalizado):
+	if(_alpha_finalizado && _bounce_finalizado):
 		OnQuadrinhoFinalizado.emit()
