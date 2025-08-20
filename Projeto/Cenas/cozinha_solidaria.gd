8ec3b8b65898e77cc578ccf2155448ea0eaa44cb
Alef Janguas da Costa <alefjcosta@gmail.com>
2025-08-20 19:01:29 -0300

    Montagem do Cenário com Artes
    Montei o tscn da cozinha solidaria com as artes e com os scripts ja estabelecidos no cenario 1.

--- Projeto/Cenas/cozinha_solidaria.gd
+++ Projeto/Cenas/cozinha_solidaria.gd
@@ -1,13 +1,76 @@
-class_name CozinhaSolidaria extends Node2D
+class_name Cozinha_Solidaria extends Node2D
 
 @onready var botao_acao: TouchScreenButton = $BotaoAcao
+@onready var botao_pausa: TouchScreenButton = $BotaoPausa
+@onready var pausar: MenuPausa = $CanvasLayer/pausar
+@onready var save_agent: SaveAgent = $SaveAgent
+@onready var bt_player: BTPlayer = $BTPlayer
+@onready var botao_fim: TouchScreenButton = $BotaoFim
+@onready var briefing: Briefing = $Briefing
+
+var travar_dialogos = true
 
 func _ready() -> void:
-	ControleDeFase.carregar_nivel(1)
+	ControleDeFase.carregar_nivel()
 	ControleDeFase.iniciar_nivel()
-
+	ControleDeAudio.toca_musica_com_intro("casa_intro", "casa_loop")
+	ControleDeFase.prato_entregue.connect(_prato_entregue)
+	ControleDeFase.cena_final.connect(_fim_cena)
+	pausar.hide()
+	save_agent.SaveSceneData()
+	briefing.Iniciar()	
+	
 func _on_player_acao_ativada() -> void:
 	botao_acao.visible = true
 
 func _on_player_acao_desativada() -> void:
 	botao_acao.visible = false
+
+#essa função pausa o jogo ao pressionar o botão
+func _on_botao_pausa_pressed() -> void:
+	get_tree().paused = true
+	botao_pausa.visible = false
+	travar_dialogos = true
+	pausar.show()
+
+func _on_pausar_continuar() -> void:
+	botao_pausa.visible = true
+	travar_dialogos = false
+
+func abrir_dialogo(estado_fase: int, linha_dialogo: String) -> int:
+	if travar_dialogos:
+		return estado_fase
+	ControleDeFase.jogador.iniciar_dialogo(load("res://Dialogo/Fase00.dialogue"), linha_dialogo)
+	return estado_fase + 1
+
+func _on_player_acao_agarrou(_objeto: Variant) -> void:
+	if bt_player.blackboard.get_var('estado_fase') == 1:
+		bt_player.blackboard.set_var('estado_fase', 2)
+
+func _prato_entregue(_prato):
+	if ControleDeFase.estado_nivel.pratos_entregues.size() != 2:
+		return
+	if bt_player.blackboard.get_var('estado_fase') == 3:
+		bt_player.blackboard.set_var('estado_fase', 4)
+
+func _fim_cena(_nivel, _estado_nivel):
+	ControleDeFase.congelar_tempo()
+	ControleDeFase.estado_nivel.cena_final_iniciada = true
+	bt_player.blackboard.set_var('estado_fase', 6)
+
+func _finalizar_nivel():
+	ControleDeFase.estado_nivel.cena_final_concluida = true
+
+func _on_briefing_iniciado() -> void:
+	ControleDeFase.congelar_tempo()
+	travar_dialogos = true
+
+func _on_briefing_finalizado() -> void:	
+	ControleDeFase.descongelar_tempo()
+	travar_dialogos = false
+
+func _mostrar_botao_fim():
+	botao_fim.visible = true
+
+func _on_botao_fim_pressed() -> void:
+	_finalizar_nivel()	
