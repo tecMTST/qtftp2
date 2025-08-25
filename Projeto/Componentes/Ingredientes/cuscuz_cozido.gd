06431ab23649c540e431bbb6059b6f201eba3b15
Breno G. de Oliveira <garu@cpan.org>
2025-08-24 23:17:59 -0300

    evidencia erro quando ingrediente não é encontrado

--- Projeto/Componentes/Ingredientes/cuscuz_cozido.gd
+++ Projeto/Componentes/Ingredientes/cuscuz_cozido.gd
@@ -10,12 +10,13 @@ var esta_desenformando = false
 
 func _ready() -> void:
 	var ingredientes = Globais.ingredientes.filter(
-		func(item : Ingrediente): return item.id == 5 and item.variacao == 3)
-	if len(ingredientes) > 0:
-		ingrediente = ingredientes[0]
-		nome = ingrediente.nome
-		descricao = ingrediente.descricao
-		timer.start(tempo_ate_queimar)
+		func(item: Ingrediente): return item.id == 5 and item.variacao == 3
+	)
+	assert(len(ingredientes) > 0, "ingrediente não encontrado!")
+	ingrediente = ingredientes[0]
+	nome = ingrediente.nome
+	descricao = ingrediente.descricao
+	timer.start(tempo_ate_queimar)
 
 
 func _on_timer_timeout():
