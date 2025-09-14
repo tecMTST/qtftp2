fc494a1f1209f94e1e2cc3bd270205e70ae09452
Alef Janguas da Costa <alefjcosta@gmail.com>
2025-09-14 20:39:22 -0300

    menu pausa versao com abas 1 versao

--- Projeto/UI/Menus/menu_pausa.gd
+++ Projeto/UI/Menus/menu_pausa.gd
@@ -11,6 +11,8 @@ signal continuar
 
 func atualizar() -> void:
 	fase.text=ControleDeFase.nivel_atual.nome
+	print(ControleDeFase.receita_selecionada)
+	receita_atual.icon=load(ControleDeFase.receita_selecionada)
 	botao_briefing.icon=load(ControleDeFase.receita_selecionada.caminho_sprite)
 	
 
@@ -31,9 +33,18 @@ func _on_retornar_para_tela_inicial_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://UI/Menus/menu_principal.tscn")
 
-#abre o briefing novamente
+#abre a receita atual
 func _on_briefing_pressed() -> void:
 	if(ControleDeFase.receita_selecionada):
 		briefing.iniciar(false, ControleDeFase.receita_selecionada.nome)
 	else:
 		briefing.iniciar(false)
+
+#abre a aba de controles
+func _on_control_pressed() -> void:
+	briefing.iniciar(false, "controles")
+
+#abre a aba de controles
+func _on_config_pressed() -> void:
+	##Essa seria a tela de configurações de volumes q hj em dia eh a tela inicial, nao sei como alterar isso no momento
+	return
