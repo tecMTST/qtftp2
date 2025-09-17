a06a314b80e18ea48c8cb1659d9f8b097878cc59
Breno G. de Oliveira <garu@cpan.org>
2025-09-17 16:47:15 -0300

    implementa filas de entrega na cozinha solidária

--- Projeto/Componentes/Casa/Interagiveis/mesa_filha.gd
+++ Projeto/Componentes/Casa/Interagiveis/mesa_filha.gd
@@ -1,5 +1,7 @@
 class_name MesaFilha extends BaseInteragivel
 
+@export var fila: Node
+
 @onready var pivot: Node2D = $Pivot
 @onready var indicador: Sprite2D = $Indicador
 
@@ -15,6 +17,7 @@ func _interagir(jogador: Player) -> void:
 		jogador.objeto_agarrado != null
 		and jogador.objeto_agarrado is IngredienteBase
 		and jogador.objeto_agarrado.ingrediente.acoes[0].alvo == "mesa"
+		and (!is_instance_valid(fila) or fila.remove_pessoa_da_fila())
 	):
 		var objeto_atual = jogador.objeto_agarrado
 		jogador.soltar()
