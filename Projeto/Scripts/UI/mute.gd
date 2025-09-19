1c55001576c92d7c947077b8825eccc2a224b493
Breno G. de Oliveira <garu@cpan.org>
2025-09-19 15:37:55 -0300

    ajustes de audio na tela de opções

--- Projeto/Scripts/UI/mute.gd
+++
@@ -1,7 +0,0 @@
-extends CheckButton
-
-
-var audio_on = AudioServer.get_bus_index("Master")
-
-func _on_pressed() -> void:
-	AudioServer.set_bus_mute(audio_on, not AudioServer.is_bus_mute(audio_on))
