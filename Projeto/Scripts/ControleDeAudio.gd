7d4b366d5a9a4c2cd132ce6d8d851fc45b8f9521
Pablo R. de Melo <142453576+PabloRMelo@users.noreply.github.com>
2025-09-21 12:59:38 +0200

    tela splash terminada
    Dois logos adicionados à tela splash com transiçao mais suave para ambos

--- Projeto/Scripts/ControleDeAudio.gd
+++ Projeto/Scripts/ControleDeAudio.gd
@@ -22,6 +22,13 @@ var biblioteca_de_audio := {
 		"fogao_ligar": preload("res://Recursos/Audio/Efeitos/fogao_ligar.ogg"),
 		"fogao_cozinhando": preload("res://Recursos/Audio/Efeitos/fogao_cozinhando_loop.ogg"),
 		"fogao_alarme": preload("res://Recursos/Audio/Efeitos/fogao_alarme.ogg"),
+<<<<<<< Updated upstream
+=======
+		"clique": preload("res://Recursos/Audio/Efeitos/click.ogg"),
+		"hidratar": preload("res://Recursos/Audio/Efeitos/hidratar.ogg"),
+		"pegar_item": preload("res://Recursos/Audio/Efeitos/pegar_item.ogg"),
+		"alimento_servido": preload("res://Recursos/Audio/Efeitos/alimento_servido.ogg"),
+>>>>>>> Stashed changes
 		"splash_screen": preload("res://Recursos/Audio/Efeitos/splash.ogg"),
 		"passos": [
 			preload("res://Recursos/Audio/Efeitos/passo1.ogg"),
