e51657164cc54d1e661ae6192c875dc52227def8
Bruno Amá Stephan <brunoastephan@gmail.com>
2025-09-24 23:40:00 -0300

    continua finalizacao da cutscene fim de fase tutorial

--- Projeto/Cenas/cutscene_fimdefase.gd
+++ Projeto/Cenas/cutscene_fimdefase.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 @export var timeline : DialogicTimeline
 
-@onready var player: Player = $Personagens/Player
-@onready var filha: Filha = $Personagens/Filha
-
 func _ready() -> void:
 	var layout = Dialogic.start(timeline)
 	if layout.has_method("register_character"):
@@ -16,8 +13,3 @@ func _ready() -> void:
 func evento_dialogic(acao : String):
 	var quem = acao.split(":")[0]
 	var onde = acao.split(":")[1]
-	match quem:
-		"carol":
-			filha.mover_para_waypoint(onde)
-		"elza":
-			pass
