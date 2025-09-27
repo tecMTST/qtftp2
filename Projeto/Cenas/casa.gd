d15b3bbf3d785f3350e7e35489d5ac83cd525e46
Mihailov Vasilievic Filho <mihailov.vf@gmail.com>
2025-09-26 23:34:59 -0300

    Skip para os diálogos ingame

--- Projeto/Cenas/casa.gd
+++ Projeto/Cenas/casa.gd
@@ -19,7 +19,6 @@ func _ready() -> void:
 	briefing.iniciar()
 
 func _process(_delta: float) -> void:
-	botao_pausa.visible = not ControleDeFase.esta_dialogando
 	touch_analog.Enabled = player.ativo
 
 func _on_player_acao_ativada() -> void:
