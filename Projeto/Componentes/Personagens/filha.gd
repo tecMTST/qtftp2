d9c00b4ac878ebbd196cb9078a4b61e8d18edbae
Breno G. de Oliveira <garu@cpan.org>
2025-10-01 01:33:16 -0300

    linter

--- Projeto/Componentes/Personagens/filha.gd
+++ Projeto/Componentes/Personagens/filha.gd
@@ -30,8 +30,11 @@ func _process(delta):
 	if not cutscene:
 		if not ControleDeFase.nivel_atual or not ControleDeFase.nivel_atual.bagunca:
 			return
-	if not (target and global_position.distance_to(target.global_position) \
-	< agent.target_desired_distance):
+	if not (
+		target and global_position.distance_to(
+			target.global_position
+		) < agent.target_desired_distance
+	):
 		move_along_path(delta)
 	else:
 		velocity = Vector2.ZERO
